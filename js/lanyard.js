9fff37de67bc9265e8c26ab354790972358084ff
cart96 <icecat696@proton.me>
2022-12-21 01:57:58 +0300

    switch to websocket

--- js/lanyard.js
+++ js/lanyard.js
@@ -1,199 +1,195 @@
-API_URL = 'https://api.lanyard.rest/v1';
-USERID = '374220001743208459';
+const LANYARD_WS = 'wss://api.lanyard.rest/socket';
+const LANYARD_OP = {
+  PRESENCE: 0,
+  HELLO: 1,
+  INITIALIZE: 2,
+  HEARTBEAT: 3,
+};
+const EVENTS_TO_CALLBACK = ['INIT_STATE', 'PRESENCE_UPDATE'];
+const DISCORD_ID = '374220001743208459';
 
-// https://api.lanyard.rest/v1/users/374220001743208459
+let currentTimer = null;
 
-async function fetchResponse(userId) {
-    try {
-        const url = await fetch(`${API_URL}/users/${userId}`);
-        const response = await url.json();
-        return response;
-    } catch (error) {
-        console.error(error);
+async function initializeSocket() {
+  let ws = new WebSocket(LANYARD_WS);
+
+  ws.onmessage = ({ data }) => {
+    const received = JSON.parse(data);
+
+    switch (received.op) {
+      case LANYARD_OP.HELLO: {
+        ws.send(JSON.stringify({ op: LANYARD_OP.INITIALIZE, d: { subscribe_to_id: DISCORD_ID } }));
+
+        setInterval(() => {
+          ws.send(JSON.stringify({ op: LANYARD_OP.HEARTBEAT }));
+        }, 1000 * 30);
+      }
+
+      case LANYARD_OP.PRESENCE: {
+        if (EVENTS_TO_CALLBACK.includes(received.t)) {
+          setPresence(received.d);
+        }
+      }
     }
+  };
 
+  ws.onclose = () => initializeSocket;
 }
 
+async function setAvatar({ discord_user }) {
+  var fullUrl = `https://cdn.discordapp.com/avatars/${DISCORD_ID}/${discord_user.avatar}`;
 
-async function setAvatar() {
-    const response = await fetchResponse(USERID);
-
-    var avatarId = response.data.discord_user.avatar;
-    var fullUrl = `https://cdn.discordapp.com/avatars/${USERID}/${avatarId}`;
-
-    document.getElementById('pfp').src = fullUrl;
+  document.getElementById('pfp').src = fullUrl;
 }
 
-async function setAvatarFrame() {
-    const response = await fetchResponse(USERID);
-    const activity2 = document.getElementById('status2');
+async function setAvatarFrame({ discord_status }) {
+  const activity2 = document.getElementById('status2');
 
-    if (response.data.discord_status == 'offline') {
-        activity2.innerHTML = "I'm offline";
-        activity2.style.cssText = 'color: unset; opacity: 0.5;';
-    } else {
-        activity2.innerHTML = "I'm online";
-        activity2.style.cssText = 'color: #3ba45d; opacity: 1;';
-    }
-    switch (response.data.discord_status) {
-        case 'online':
-            document.getElementById('activity-dot').style.background =
-                '#3ba45d';
-            document.getElementById('activity-dot').title = 'Online';
-            activity2.innerHTML = 'Online';
-            break;
+  if (discord_status == 'offline') {
+    activity2.innerHTML = "I'm offline";
+    activity2.style.cssText = 'color: unset; opacity: 0.5;';
+  } else {
+    activity2.innerHTML = "I'm online";
+    activity2.style.cssText = 'color: #3ba45d; opacity: 1;';
+  }
+  switch (discord_status) {
+    case 'online':
+      document.getElementById('activity-dot').style.background = '#3ba45d';
+      document.getElementById('activity-dot').title = 'Online';
+      activity2.innerHTML = 'Online';
+      break;
 
-        case 'dnd':
-            document.getElementById('activity-dot').style.background =
-                '#ed4245';
-            document.getElementById('activity-dot').title = 'Do not disturb';
-            break;
+    case 'dnd':
+      document.getElementById('activity-dot').style.background = '#ed4245';
+      document.getElementById('activity-dot').title = 'Do not disturb';
+      break;
 
-        case 'idle':
-            document.getElementById('activity-dot').style.background =
-                '#faa81a';
-            document.getElementById('activity-dot').title = 'Idle';
-            break;
+    case 'idle':
+      document.getElementById('activity-dot').style.background = '#faa81a';
+      document.getElementById('activity-dot').title = 'Idle';
+      break;
 
-        case 'offline':
-            document.getElementById('activity-dot').style.background =
-                '#747e8c';
-            document.getElementById('activity-dot').title = 'Offline';
-            break;
-    }
+    case 'offline':
+      document.getElementById('activity-dot').style.background = '#747e8c';
+      document.getElementById('activity-dot').title = 'Offline';
+      break;
+  }
 }
 
-async function setUsername() {
-    const response = await fetchResponse(USERID);
+async function setUsername({ discord_user }) {
+  var { username, discriminator } = discord_user;
+  var fullName = `${username}#${discriminator}`;
 
-    var user = response.data.discord_user.username;
-    var discriminator = response.data.discord_user.discriminator;
-    var fullName = `${user}#${discriminator}`;
-
-    document.getElementById('username').innerHTML = fullName;
+  document.getElementById('username').innerHTML = fullName;
 }
 
-async function setStatus() {
-    const response = await fetchResponse(USERID);
+async function setStatus({ discord_status, activities }) {
+  if (discord_status == 'offline') {
+    return;
+  }
 
-    if (response.data.discord_status == 'offline') {
-        return;
-    }
-    var status = response.data.activities[0].state;
-    if (!status) {
-        return;
-    }
-    document.getElementById('status').innerHTML = `Status: "${status}"`;
+  var status = activities[0].state;
+  if (!status) {
+    return;
+  }
+
+  document.getElementById('status').innerHTML = `Status: "${status}"`;
 }
 
-async function setSpotifyBar() {
-    const response = await fetchResponse(USERID);
+async function setSpotifyBar({ listening_to_spotify, spotify }) {
+  var bar = document.getElementById('spotify-innerbar');
+  var bar2 = document.getElementById('spotify-time-end');
+  var bar3 = document.getElementById('spotify-time-start');
 
-    var bar = document.getElementById('spotify-innerbar');
-    var bar2 = document.getElementById('spotify-time-end');
-    var bar3 = document.getElementById('spotify-time-start');
-    
-    if (response.data.listening_to_spotify == false) {
-        bar.style.display = 'none';
-        bar2.innerHTML = '00:00';
-        bar3.innerHTML = '00:00';
-        return;
-    }
+  if (listening_to_spotify == false) {
+    bar.style.display = 'none';
+    bar2.innerHTML = '00:00';
+    bar3.innerHTML = '00:00';
+    return;
+  }
 
+  const end = spotify.timestamps.end;
+  const start = spotify.timestamps.start;
+
+  let duration = end - start;
+
+  function spotifyTimeSet(date, element) {
+    const x = document.getElementById(element);
+    const y = new Date(date);
+
+    const minutes = y.getMinutes();
+    const seconds = y.getSeconds();
+    const formmatedseconds = seconds < 10 ? `0${seconds}` : seconds;
+    x.innerHTML = `${minutes}:${formmatedseconds}`;
+  }
+
+  spotifyTimeSet(duration, 'spotify-time-end');
+
+  currentTimer = setInterval(() => {
     const date = new Date().getTime();
+    let current = date - start;
 
-    const v1 =
-        response.data.spotify.timestamps.end -
-        response.data.spotify.timestamps.start;
-    const v2 = date - response.data.spotify.timestamps.start;
-
-
-    function spotifyTimeSet(date, element) {
-        const x = document.getElementById(element);
-        const y = new Date(date);
-
-        const minutes = y.getMinutes();
-        const seconds = y.getSeconds();
-        const formmatedseconds = seconds < 10 ? `0${seconds}` : seconds;
-        x.innerHTML = `${minutes}:${formmatedseconds}`;
+    if (current >= duration) {
+      clearInterval(currentTimer);
     }
 
-    spotifyTimeSet(v1, 'spotify-time-end');
-    spotifyTimeSet(v2, 'spotify-time-start');
+    spotifyTimeSet(current, 'spotify-time-start');
 
-    prcnt = (v2 / v1) * 100;
-
+    prcnt = (current / duration) * 100;
     precentage = Math.trunc(prcnt);
-
     prccc = Math.round((prcnt + Number.EPSILON) * 100) / 100;
     i = 1;
 
     bar.style.display = 'block';
     bar.style.width = prccc + '%';
+  }, 1000);
 }
 
-async function setSpotifySongName() {
-    const response = await fetchResponse(USERID);
-    var par = document.getElementById('spotify-song');
+async function setSpotifySongName({ listening_to_spotify, spotify }) {
+  var par = document.getElementById('spotify-song');
 
-    if (response.data.listening_to_spotify == false) {
-        par.innerHTML = 'Nothing is playing';
-        return;
-    }
+  if (listening_to_spotify == false) {
+    par.innerHTML = 'Nothing is playing';
+    return;
+  }
 
-    var songName = response.data.spotify.song;
-
-    var fullName = songName;
-    par.style.display = 'block';
-    par.innerHTML = fullName;
+  par.style.display = 'block';
+  par.innerHTML = spotify.song;
 }
 
-async function setSpotifyAlbumCover() {
-    const response = await fetchResponse(USERID);
-    var par = document.getElementById('album-cover');
+async function setSpotifyAlbumCover({ listening_to_spotify, spotify }) {
+  var par = document.getElementById('album-cover');
 
-    if (response.data.listening_to_spotify == false) {
-        par.style.display = 'none';
-        return;
-    }
+  if (listening_to_spotify == false) {
+    par.style.display = 'none';
+    return;
+  }
 
-    var albumcover = response.data.spotify.album_art_url;
-    par.style.display = 'block';
-    par.src = albumcover;
+  par.style.display = 'block';
+  par.src = spotify.album_art_url;
 }
 
-async function setSpotifyArtist() {
-    const response = await fetchResponse(USERID);
-    var par = document.getElementById('spotify-artist');
+async function setSpotifyArtist({ listening_to_spotify, spotify }) {
+  var par = document.getElementById('spotify-artist');
 
-    if (response.data.listening_to_spotify == false) {
-        par.innerHTML = 'Nobody';
-        return;
-    }
+  if (listening_to_spotify == false) {
+    par.innerHTML = 'Nobody';
+    return;
+  }
 
-    var artistName = response.data.spotify.artist;
-
-    par.style.display = 'block';
-    par.innerHTML = `by: ${artistName}`;
+  par.style.display = 'block';
+  par.innerHTML = `by: ${spotify.artist}`;
 }
 
-async function presenceInvoke() {
-    setSpotifyAlbumCover();
-    setSpotifyArtist();
-    setSpotifySongName();
-    setSpotifyBar();
+async function setPresence(data) {
+  setUsername(data);
+  setStatus(data);
+  setAvatarFrame(data);
+  setAvatar(data);
+  setSpotifyAlbumCover(data);
+  setSpotifyArtist(data);
+  setSpotifySongName(data);
+  setSpotifyBar(data);
 }
 
-async function statusInvoke() {
-    setStatus();
-    setAvatarFrame();
-}
-
-async function invoke() {
-    setInterval(presenceInvoke, 1000);
-    setAvatar();
-    setUsername();
-    setInterval(statusInvoke, 1000);
-}
-
-invoke();
+initializeSocket();
